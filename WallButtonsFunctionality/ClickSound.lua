2b5704680d67fce2b82799e0340592699f45878c
paulocezarnbr <69541563+paulocezarnbr@users.noreply.github.com>
2023-06-18 21:56:45 -0300

    Update ClickSound.lua

--- WallButtonsFunctionality/ClickSound.lua
+++ WallButtonsFunctionality/ClickSound.lua
@@ -1,15 +1,17 @@
+--[[
+
+Clippsly "Mobile Dock" Click Sound
+By: Placeholder Studio.
+Twitter: @Clippsly
+EDITED BY PAULOCEZARNBR
+]]
+
 -- Get reference to the Dock frame
 local dock = script.Parent.Parent.Wallpaper
 
--- Define the jump scale for the buttons
-local jumpScale = 0.2
-
--- Define the jump duration
-local jumpDuration = 0.1
-
 -- Set up the click event handlers for each button
 for _, button in ipairs(dock:GetChildren()) do
-	if button:IsA("ImageButton") then
+	if button:IsA("GuiButton") then
 		button.MouseButton1Click:Connect(function()
 			-- Create the jump tween
 			local Click = script.Click
